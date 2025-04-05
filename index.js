ce8d81bfb26628b58048622db3c9db13c055c6a3
Chandisa Randeni <chandisarandeni2003@gmail.com>
2025-04-05 17:16:52 +0530

    Establish MongoDB connection

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+//import routes
+import userRoute from "./routes/userRoute.js";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 const port = 3000;
+
+// Middleware to parse JSON request body
+app.use(express.json());
+
+// MongoDB connection setup
+const mongoURI = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+//use routes
+app.use("/register", userRoute);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
