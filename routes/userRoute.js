18e2067ca49e5784082a2450e2604acb419b95b5
chandisarandeni <chandisarandeni2003@gmail.com>
2025-04-06 10:18:36 +0530

    Add JWT auth for user registration and login
    Implement user registration and login functionality with JWT authentication

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,11 +2,13 @@ import express from "express";
 
 //import routes
 import { registerUser } from "../controllers/userController.js";
+import { loginUser } from "../controllers/userController.js";
 
 // Create a new router instance
 const router = express.Router();
 
 // Route to register a new user
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
